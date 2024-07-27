15af98da2cf52b9bcec2b77ab9ca3ff635452832
Mirabie <mirmdshabie@gmail.com>
2024-07-28 03:04:01 +0600

    first changes

--- 01-viralfacts-kawsar870/js/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('clickButton');
-    button.addEventListener('click', () => {
-        alert('Button Clicked!');
-    });
-});
